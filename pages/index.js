9651a21f6f198a26d53087b82546886fe57e0632
AndersonPM <rhcpandersonph@gmail.com>
2024-12-06 22:50:03 -0300

    Flores

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home() {
-    return <h1>Camomila minha linda, I love you!!! :) 
-        <img src="flores.jpg" alt="flores" ></img>
-    </h1>
+    return '<h1>Camomila minha linda, I love you!!! :)</h1><body><img src="flores.jpg" alt="flores" ></img></body>'
+ 
 }
 
 export default Home;
